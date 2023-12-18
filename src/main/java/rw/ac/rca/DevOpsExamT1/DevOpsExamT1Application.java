3b3dc6cd11f7015c60ce5ea206ce76abe9b16fdf
israel_manzi <104481099+israelmanzi@users.noreply.github.com>
2023-12-18 08:55:15 +0200

    initial commit

---
+++ src/main/java/rw/ac/rca/DevOpsExamT1/DevOpsExamT1Application.java
@@ -0,0 +1,13 @@
+package rw.ac.rca.DevOpsExamT1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsExamT1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsExamT1Application.class, args);
+	}
+
+}
---
+++ src/test/java/rw/ac/rca/DevOpsExamT1/DevOpsExamT1ApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.ac.rca.DevOpsExamT1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsExamT1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
